1e1e9d58cf6fbfb845b6dfb21bf597c00806377a
Baldezhnik232 <stanislav.bondarev.2015@gmail.com>
2026-03-18 23:16:33 +0300

    Fetching Data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -41,7 +41,7 @@ export async function fetchLatestInvoices() {
 
     const latestInvoices = data.map((invoice) => ({
       ...invoice,
-      amount: formatCurrency(invoice.amount),
+      amount: invoice.amount,
     }));
     return latestInvoices;
   } catch (error) {
